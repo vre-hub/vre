734b5c3a75d75f8fe6db3ac70ab683b89cb4ce56
egazzarr <56166621+egazzarr@users.noreply.github.com>
2022-12-09 13:23:14 +0000

    variables needed for rucio servers

--- iac/tf/modules/rucio/rucio-server/variables.tf
+++ iac/tf/modules/rucio/rucio-server/variables.tf
@@ -7,3 +7,20 @@ variable "release-suffix" {
   description = "The suffix added to the rucio server release"
   type        = string
 }
+
+variable "image-tag" {
+  description = "The rucio dockerhub image tag"
+  type        = string
+  default     = "release-1.29.8"
+}
+
+variable "rucio-vre-dn" {
+  description = "Domain name of service for which grid host certificates were requested"
+  type        = string
+}
+
+variable "rucio-auth-vre-dn" {
+  description = "Domain name of service for which grid host certificates were requested"
+  type        = string
+}
+
