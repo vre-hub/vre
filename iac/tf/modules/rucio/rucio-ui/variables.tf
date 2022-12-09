15c4a7aa03e7eb68931ff9844a0a86c862b1af43
egazzarr <56166621+egazzarr@users.noreply.github.com>
2022-12-09 11:55:32 +0000

    moving DN host names to main variables

--- iac/tf/modules/rucio/rucio-ui/variables.tf
+++ iac/tf/modules/rucio/rucio-ui/variables.tf
@@ -14,22 +14,5 @@ variable "image-tag" {
   default     = "release-1.29.8"
 }
 
-variable "rucio-vre-dn" {
-  description = "Domain name of service for which grid host certificates were requested"
-  type        = string
-  default     = "rucio.vre.cern.ch" #change accordingly
-}
-
-variable "rucio-auth-vre-dn" {
-  description = "Domain name of service for which grid host certificates were requested"
-  type        = string
-  default     = "rucio-auth.vre.cern.ch" #change accordingly
-}
-
-variable "rucio-ui-vre-dn" {
-  description = "Domain name of service for which grid host certificates were requested"
-  type        = string
-  default     = "rucio-ui.vre.cern.ch" #change accordingly
-}
 
 
