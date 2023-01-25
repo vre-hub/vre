5d0c94f49451452bdb240abdaf4eac62d7f41a8e
Domenic Gosein <87760323+goseind@users.noreply.github.com>
2023-01-25 17:25:12 +0100

    add new rucio-webui dev chart

--- iac/tf/cluster/main-helm.tf
+++ iac/tf/cluster/main-helm.tf
@@ -19,17 +19,22 @@ resource "helm_release" "rucio-daemons-chart" {
   }
 }
 
-/* resource "helm_release" "rucio-ui-chart" {
-  name       = "rucio-ui-${var.resource-suffix}"
+resource "helm_release" "rucio-webui-chart" {
+  name       = "rucio-webui-${var.resource-suffix}"
   repository = "https://rucio.github.io/helm-charts"
-  chart      = "rucio-ui"
+  chart      = "rucio-webui"
   version    = "1.30.0"
   namespace  = var.ns-rucio
 
   values = [
-    "${file("rucio/values-ui.yaml")}"
+    "${file("rucio/values-webui.yaml")}"
   ]
-} */
+
+  set {
+    name = "config.database.default"
+    value = data.kubernetes_secret_v1.rucio_db_secret.data.dbconnectstring
+  }
+}
 
 resource "helm_release" "rucio-server-chart" {
   name       = "rucio-server-${var.resource-suffix}"
