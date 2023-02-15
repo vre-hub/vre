2bd7a8a4666fa591bcb87c8d7476a6fd95863ef0
Enrique <enrique.garcia.garcia@cern.ch>
2023-02-15 16:57:01 +0100

    add encrypted files plus script

---
+++ iac/scripts/create_rucio_secrets-rses.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# S3 storage configuration needs to be applied to the RUCIO serves, following RUCIO documentation
+#  http://rucio.cern.ch/documentation/s3_rse_config/
+
+HELM_RELEASE_SERVER="rucio-server-cvre"
+HELM_RELEASE_DAEMONS="rucio-daemons-cvre"
+
+CONTROLLER_NS="shared-services"
+CONTROLLER_NAME="sealed-secrets-cvre"
+
+# rucio namespace
+RUCIO_NS="rucio"
+
+# sealed secret output yaml prefix
+YAML_PRFX="ss_"
+
+SECRETS_STORE="../secrets/rucio/"
+
+kubectl create secret generic ${HELM_RELEASE_SERVER}-rse-accounts --dry-run=client --from-file=rse-s3_config.json -o yaml \
+| kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${RUCIO_NS} > ${SECRETS_STORE}${YAML_PRFX}${HELM_RELEASE_SERVER}-rse-accounts.yaml
+kubectl apply -f ${SECRETS_STORE}${YAML_PRFX}${HELM_RELEASE_SERVER}-rse-accounts.yaml
+
+kubectl create secret generic ${HELM_RELEASE_DAEMONS}-rse-accounts --dry-run=client --from-file=rse-s3_config.json -o yaml \
+| kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${RUCIO_NS} > ${SECRETS_STORE}${YAML_PRFX}${HELM_RELEASE_DAEMONS}-rse-accounts.yaml
+kubectl apply -f ${SECRETS_STORE}${YAML_PRFX}${HELM_RELEASE_DAEMONS}-rse-accounts.yaml
