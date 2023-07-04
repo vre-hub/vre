462921693cfc40fa0f95d1f619b7480a8bde74d0
egazzarr <56166621+egazzarr@users.noreply.github.com>
2023-07-04 13:01:48 +0200

    Update update_rucio_proxies.sh

--- infrastructure/scripts/update_rucio_proxies.sh
+++ infrastructure/scripts/update_rucio_proxies.sh
@@ -2,12 +2,12 @@
 
 ## PURPOSE
 ## script to update the proxy certificates for the rucio daemons. 
-## The /x509 file gets created every 8 hours (the suration is 12 hours) from the command voms-proxy-init on the x509 certificate 
+## The /tmp/x509 file gets created every 8 hours (the suration is 12 hours) from the command voms-proxy-init on the x509 certificate 
 ## and it is used y the rucio daemons to perform the transfers with the File Transfer Service (FTS). 
 
 # this command makes sure that the script stops if something goes bad as everything runs in the background
 set -e 
- 
+
 PROXYPATH="/afs/cern.ch/user/e/egazzarr/private/clusters/vre-cluster/proxies/"
 RELEASENAMEDAEMONS="daemons-vre"
 RUCIO_NS="rucio-vre"
@@ -23,31 +23,25 @@ export ROBOT_CLIENT_KEY="/afs/cern.ch/user/e/egazzarr/private/clusters/vre-clust
 export X509_USER_PROXY="/afs/cern.ch/user/e/egazzarr/private/clusters/vre-cluster/proxies/x509up_escape"
 
 # copying the robot client certificate in each daemon pod (had to exclude the automatic restart)
-cp ${X509_USER_PROXY} x509up 
-for OUTPUT in $(kubectl -n ${RUCIO_NS} get pods | cu-d " " -f1 | grep daemons | grep -vw 'automatic' | grep -vw 'renew-fts-proxy')
-do
-    echo $OUTPUT
-    kubectl -n ${RUCIO_NS} cp x509up $OUTPUT:/x509up
-done
+
+cp ${X509_USER_PROXY} x509up
+
+# for OUTPUT in $(kubectl -n ${RUCIO_NS} get pods | cut -d " " -f1 | grep daemons | grep -vw 'automatic' | grep -vw 'renew-fts-proxy')
+# do
+#   echo $OUTPUT
+#   kubectl -n ${RUCIO_NS} cp x509up $OUTPUT:/tmp/x509up
+# done
 
 # rucio daemons x509_up secret
 kubectl delete secret ${RELEASENAMEDAEMONS}-rucio-x509up -n ${RUCIO_NS}
 kubectl create secret generic ${RELEASENAMEDAEMONS}-rucio-x509up --from-file=x509up -n ${RUCIO_NS}
 
-# x509up for Crons
-#kubectl delete secret prod-rucio-x509up -n crons
-#kubectl create secret generic prod-rucio-x509up --from-file=x509up -n crons
-#kubectl delete secret prod-rucio-x509up
-#kubectl create secret generic prod-rucio-x509up --from-file=x509up
-
 kubectl delete secret prod-rucio-x509up -n ${RUCIO_NS}
 kubectl create secret generic prod-rucio-x509up --from-file=x509up -n ${RUCIO_NS}
 
-# FTS delegation (needed?) renewed
-ssh lxplus fts-rest-delegate -s https://fts3-pilot.cern.ch:8446 --verbose --key ${ROBOT_CLIENT_KEY} --cert ${ROBOT_CLIENT_CERT}
-
-#FTS delegation old only on lxplus
-#ssh lxplus fts-delegation-init -s https://fts3-pilot.cern.ch:8446 --proxy ${X509_USER_PROXY}
+# FTS delegation (IMPORTANT)
+ssh lxplus fts-rest-delegate -s https://fts3-pilot.cern.ch:8446 --verbose --cert ${X509_USER_PROXY}
 
 exit 0
+
  
