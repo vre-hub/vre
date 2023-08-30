e781ecd5ac6735d4127258202d990e53f244e12a
Elena Gazzarrini <gazzarrini.elena@gmail.com>
2023-08-30 11:23:21 +0200

    black ran on python files

--- containers/iam-reana-sync/add_reana_users.py
+++ containers/iam-reana-sync/add_reana_users.py
@@ -2,15 +2,20 @@ import json
 import os
 import subprocess
 
-client_id = os.environ['CLIENT_ID']
-client_secret  = os.environ['CLIENT_SECRET']  
-reana_admin_token = os.environ['REANA_ADMIN_TOKEN']
+client_id = os.environ["CLIENT_ID"]
+client_secret = os.environ["CLIENT_SECRET"]
+reana_admin_token = os.environ["REANA_ADMIN_TOKEN"]
 
-with open('/home/emails.json', 'r') as file:
+with open("/home/emails.json", "r") as file:
     j = json.load(file)
 for i in j:
     try:
-        subprocess.check_output([f'flask reana-admin user-create --email {i} --admin-access-token {reana_admin_token}'], shell=True, encoding='utf-8')
+        subprocess.check_output(
+            [
+                f"flask reana-admin user-create --email {i} --admin-access-token {reana_admin_token}"
+            ],
+            shell=True,
+            encoding="utf-8",
+        )
     except subprocess.CalledProcessError as e:
         print(e.stderr)
-   
