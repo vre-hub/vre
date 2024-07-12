f776c5fe1b0cbc80692c619664486e42ec78b1a7
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-07-12 11:37:22 +0200

    update secret script and apply jhub share secrets

--- infrastructure/scripts/jhub_secrets.sh
+++ infrastructure/scripts/jhub_secrets.sh
@@ -4,15 +4,21 @@ RAW_SECRETS_TMP_DIR="/root/software/vre/infrastructure/secrets/tmp_local_secrets
 SECRETS_STORE="/root/software/vre/infrastructure/secrets/jhub"
 NAMESPACE="jhub"
 
-SECRET_NAME="jhub-vre-iam-secrets.yaml"
-SECRET_FILE=${RAW_SECRETS_TMP_DIR}/${SECRET_NAME}
-echo "Creating ${SECRET_NAME} secret"
-cat $SECRET_FILE | kubeseal --controller-name=sealed-secrets-controller --controller-namespace=sealed-secrets --format yaml --namespace=${NAMESPACE} > ${SECRETS_STORE}/ss_${SECRET_NAME}
-kubectl apply -f ${SECRETS_STORE}/ss_${SECRET_NAME}
+SECRET_FILE="jhub-vre-iam-secrets.yaml"
+SECRET_FULL_PATH=${RAW_SECRETS_TMP_DIR}/${SECRET_FILE}
+echo "Creating ${SECRET_FILE} secret"
+cat $SECRET_FULL_PATH | kubeseal --controller-name=sealed-secrets-controller --controller-namespace=sealed-secrets --format yaml --namespace=${NAMESPACE} > ${SECRETS_STORE}/ss_${SECRET_FILE}
+kubectl apply -f ${SECRETS_STORE}/ss_${SECRET_FILE}
 
 
-SECRET_NAME="jhub-vre-db.yaml"
-SECRET_FILE=${RAW_SECRETS_TMP_DIR}/${SECRET_NAME}
-echo "Creating ${SECRET_NAME} secret"
-cat $SECRET_FILE | kubeseal --controller-name=sealed-secrets-controller --controller-namespace=sealed-secrets --format yaml --namespace=${NAMESPACE} > ${SECRETS_STORE}/ss_${SECRET_NAME}
-kubectl apply -f ${SECRETS_STORE}/ss_${SECRET_NAME}
+SECRET_FILE="jhub-vre-db.yaml"
+SECRET_FULL_PATH=${RAW_SECRETS_TMP_DIR}/${SECRET_FILE}
+echo "Creating ${SECRET_FILE} secret"
+cat $SECRET_FULL_PATH | kubeseal --controller-name=sealed-secrets-controller --controller-namespace=sealed-secrets --format yaml --namespace=${NAMESPACE} > ${SECRETS_STORE}/ss_${SECRET_FILE}
+kubectl apply -f ${SECRETS_STORE}/ss_${SECRET_FILE}
+
+SECRET_FILE="jhub-vre-share.yaml"
+SECRET_FULL_PATH=${RAW_SECRETS_TMP_DIR}/${SECRET_FILE}
+echo "Creating ${SECRET_FILE} secret"
+cat $SECRET_FULL_PATH | kubeseal --controller-name=sealed-secrets-controller --controller-namespace=sealed-secrets --format yaml --namespace=${NAMESPACE} > ${SECRETS_STORE}/ss_${SECRET_FILE}
+kubectl apply -f ${SECRETS_STORE}/ss_${SECRET_FILE}
