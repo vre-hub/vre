bb2a73ee932557d45bb085a35b48c962eae4644e
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-07-24 09:45:47 +0200

    remove vre-share-secrets as they are not needed

--- infrastructure/scripts/jhub_secrets.sh
+++ infrastructure/scripts/jhub_secrets.sh
@@ -16,9 +16,3 @@ SECRET_FULL_PATH=${RAW_SECRETS_TMP_DIR}/${SECRET_FILE}
 echo "Creating ${SECRET_FILE} secret"
 cat $SECRET_FULL_PATH | kubeseal --controller-name=sealed-secrets-controller --controller-namespace=sealed-secrets --format yaml --namespace=${NAMESPACE} > ${SECRETS_STORE}/ss_${SECRET_FILE}
 kubectl apply -f ${SECRETS_STORE}/ss_${SECRET_FILE}
-
-SECRET_FILE="jhub-vre-share.yaml"
-SECRET_FULL_PATH=${RAW_SECRETS_TMP_DIR}/${SECRET_FILE}
-echo "Creating ${SECRET_FILE} secret"
-cat $SECRET_FULL_PATH | kubeseal --controller-name=sealed-secrets-controller --controller-namespace=sealed-secrets --format yaml --namespace=${NAMESPACE} > ${SECRETS_STORE}/ss_${SECRET_FILE}
-kubectl apply -f ${SECRETS_STORE}/ss_${SECRET_FILE}
