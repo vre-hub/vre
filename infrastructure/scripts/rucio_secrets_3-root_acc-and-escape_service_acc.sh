670f6354e1947510783a2dc9da2b6b9ec5109e72
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-08-19 17:53:16 +0200

    add service account secret

--- infrastructure/scripts/rucio_secrets_3-root_acc-and-escape_service_acc.sh
+++ infrastructure/scripts/rucio_secrets_3-root_acc-and-escape_service_acc.sh
@@ -17,4 +17,11 @@ OUTPUT_SECRET="rucio-root-account.yaml"
 cat ${RAW_SECRETS} | kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${RUCIO_NS} > ${SECRETS_DIR}/ss_${OUTPUT_SECRET}
 kubectl apply -f ${SECRETS_DIR}/ss_${OUTPUT_SECRET}
 
+echo " *** Create and apply ESCAPE SERVICE ACCOUNT secret"
+
+RAW_SECRETS="/root/software/vre/infrastructure/secrets/tmp_local_secrets/escape-service-account.yaml"
+OUTPUT_SECRET="escape-service-account.yaml"
+cat ${RAW_SECRETS} | kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${RUCIO_NS} > ${SECRETS_DIR}/ss_${OUTPUT_SECRET}
+kubectl apply -f ${SECRETS_DIR}/ss_${OUTPUT_SECRET}
+
 echo "Done"
