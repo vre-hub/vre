2ec301e9bc9a347321073a0b3046efeabcbffd92
Enrique Garcia <40355845+garciagenrique@users.noreply.github.com>
2024-11-11 17:25:44 +0100

    change jhub auth_state (#289)

--- infrastructure/scripts/jhub_secrets.sh
+++ infrastructure/scripts/jhub_secrets.sh
@@ -16,3 +16,11 @@ SECRET_FULL_PATH=${RAW_SECRETS_TMP_DIR}/${SECRET_FILE}
 echo "Creating ${SECRET_FILE} secret"
 cat $SECRET_FULL_PATH | kubeseal --controller-name=sealed-secrets-controller --controller-namespace=sealed-secrets --format yaml --namespace=${NAMESPACE} > ${SECRETS_STORE}/ss_${SECRET_FILE}
 kubectl apply -f ${SECRETS_STORE}/ss_${SECRET_FILE}
+
+
+# To create a persistant auth-state for the jhub configuration
+SECRET_FILE="jhub-vre-auth-state.yaml"
+SECRET_FULL_PATH=${RAW_SECRETS_TMP_DIR}/${SECRET_FILE}
+echo "Creating ${SECRET_FILE} secret"
+cat $SECRET_FULL_PATH | kubeseal --controller-name=sealed-secrets-controller --controller-namespace=sealed-secrets --format yaml --namespace=${NAMESPACE} > ${SECRETS_STORE}/ss_${SECRET_FILE}
+kubectl apply -f ${SECRETS_STORE}/ss_${SECRET_FILE}
