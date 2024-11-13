9c22cb624a3cfbc5a22bead2faa96c550bc83520
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-11-13 12:04:36 +0100

    add idpsecrets for SERVERS too

--- infrastructure/scripts/rucio_secrets_2-servers.sh
+++ infrastructure/scripts/rucio_secrets_2-servers.sh
@@ -64,7 +64,12 @@ kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLE
 
 kubectl apply -f ${SECRETS_DIR}/ss_${HELM_RELEASE_SERVER_AUTH}-server-cafile.yaml
 
-echo " *** Create and apply OIDC secrets for SERVER AUTH"
+echo " *** Create and apply OIDC secrets for both SERVER and SERVER AUTH"
+
+kubectl create secret generic ${HELM_RELEASE_SERVER}-idpsecrets --dry-run=client --from-file=${RAW_SECRETS_IDP} -o yaml | \
+kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${RUCIO_NS} > ${SECRETS_DIR}/ss_${HELM_RELEASE_SERVER}-idpsecrets.yaml
+
+kubectl apply -f ${SECRETS_DIR}/ss_${HELM_RELEASE_SERVER}-idpsecrets.yaml
 
 kubectl create secret generic ${HELM_RELEASE_SERVER_AUTH}-idpsecrets --dry-run=client --from-file=${RAW_SECRETS_IDP} -o yaml | \
 kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${RUCIO_NS} > ${SECRETS_DIR}/ss_${HELM_RELEASE_SERVER_AUTH}-idpsecrets.yaml
