6258cffa7c262c691a64e00ac771ccc6cdcefd5b
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-11-18 16:32:59 +0100

    start reana v093 deployment on new VRE

---
+++ infrastructure/scripts/reana_secrets.sh
@@ -0,0 +1,53 @@
+##!/bin/bash
+
+echo "Start REANA secrets scripts"
+
+# kubeseal controller namespace
+CONTROLLER_NS="sealed-secrets"
+CONTROLLER_NAME="sealed-secrets-controller"
+
+# REANA namespace
+REANA_NS="reana"
+
+# Output dir
+SECRETS_DIR="/root/software/vre/infrastructure/secrets/reana"
+RAW_SECRETS_TMP_DIR="/root/software/vre/infrastructure/secrets/tmp_local_secrets"
+
+echo "Create REANA DB secret"
+
+# name of output secret to apply
+DB_OUTPUT_SECRET="reana-db-secrets.yaml"
+RAW_DB_FILE_SECRET=${RAW_SECRETS_TMP_DIR}/${DB_OUTPUT_SECRET}
+
+cat ${RAW_DB_FILE_SECRET} | kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${REANA_NS} > ${SECRETS_DIR}/ss_${DB_OUTPUT_SECRET}
+kubectl apply -f ${SECRETS_DIR}/ss_${DB_OUTPUT_SECRET}
+
+
+# echo "Create REANA Admin Account"
+
+# ADMIN_ACCOUNT_SECRET='reana-admin.yaml'
+# RAW_ADMIN_FILE_SECRET=${RAW_SECRETS_TMP_DIR}/${ADMIN_ACCOUNT_SECRET}
+
+# cat ${RAW_ADMIN_FILE_SECRET} | kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${REANA_NS} > ${SECRETS_DIR}/ss_${ADMIN_ACCOUNT_SECRET}
+# kubectl apply -f ${SECRETS_DIR}/ss_${ADMIN_ACCOUNT_SECRET}
+
+
+# echo "Create REANA IAM client credentials"
+
+# REANA_IAM_ACCOUNT_SECRET='reana-iam-secrets.yaml'
+# RAW_REANA_IAM_FILE_SECRET=${RAW_SECRETS_TMP_DIR}/${REANA_IAM_ACCOUNT_SECRET}
+
+# cat ${RAW_REANA_IAM_FILE_SECRET} | kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${REANA_NS} > ${SECRETS_DIR}/ss_${REANA_IAM_ACCOUNT_SECRET}
+# kubectl apply -f ${SECRETS_DIR}/ss_${REANA_IAM_ACCOUNT_SECRET}
+
+
+# echo "Create 'REANA secrets' secret"
+# # This secret is unknow for what is used - no doc :harold:
+# REANA_SECRETS_SECRET='reana-secrets.yaml'
+# RAW_REANA_SECRETS_FILE_SECRET=${RAW_SECRETS_TMP_DIR}/${REANA_SECRETS_SECRET}
+
+# cat ${RAW_REANA_SECRETS_FILE_SECRET} | kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${REANA_NS} > ${SECRETS_DIR}/ss_${REANA_SECRETS_SECRET}
+# kubectl apply -f ${SECRETS_DIR}/ss_${REANA_SECRETS_SECRET}
+
+
+echo "END REANA Secret Script"
