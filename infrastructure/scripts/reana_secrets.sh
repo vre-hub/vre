3d82c853d5426ccc0db5d6a72ee3843a0f58b4ce
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-11-18 16:45:16 +0100

    re appaly reana db secret with correct secret name

--- infrastructure/scripts/reana_secrets.sh
+++ infrastructure/scripts/reana_secrets.sh
@@ -16,7 +16,7 @@ RAW_SECRETS_TMP_DIR="/root/software/vre/infrastructure/secrets/tmp_local_secrets
 echo "Create REANA DB secret"
 
 # name of output secret to apply
-DB_OUTPUT_SECRET="reana-db-secrets.yaml"
+DB_OUTPUT_SECRET="reana-db.yaml"
 RAW_DB_FILE_SECRET=${RAW_SECRETS_TMP_DIR}/${DB_OUTPUT_SECRET}
 
 cat ${RAW_DB_FILE_SECRET} | kubeseal --controller-name=${CONTROLLER_NAME} --controller-namespace=${CONTROLLER_NS} --format yaml --namespace=${REANA_NS} > ${SECRETS_DIR}/ss_${DB_OUTPUT_SECRET}
